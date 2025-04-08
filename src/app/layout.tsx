6c5151367476edffcbb74eda48db9c3a12f4bf5e
Damian Radecki <damian.radecki@tsh.io>
2025-04-08 19:03:47 +0200

    chore: add tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css";
+
 import { NavBar } from "./components/navbar/navbar";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "MovieRank",
